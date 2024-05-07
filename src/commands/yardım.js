bdb2ceb0ecc87dd031927e057d6cc6160f78583e
Stones <158315694+Stones10@users.noreply.github.com>
2024-05-07 16:34:55 +0300

    yardım.js

--- src/commands/yardım.js
+++ src/commands/yardım.js
@@ -8,8 +8,8 @@ module.exports = {
     run: async (client, interaction) => {
         const Yardım = new EmbedBuilder()
             .setColor("Blurple")
-            .setImage("https://media.discordapp.net/attachments/1004368050038001804/1086953067301310524/standard_1.gif")
-            .setTitle("Fr3zy Youtube • Moderasyon Menüsü")
+            .setImage("https://media.discordapp.net/attachments/1235344694569865277/1237397037239238747/bot_gif.gif?ex=663b7f4b&is=663a2dcb&hm=609c722e418bb4f5eefccf2cd6dd1de4a3708841cdd07fa1d181346055090a3b&=")
+            .setTitle("Stones10 • Moderasyon Menüsü")
             .setDescription(`                
                 **/ban** • Belirtilen kişiyi sunucudan banlar.
                 **/kick** • Belirtilen kişiyi sunucudan atar.
